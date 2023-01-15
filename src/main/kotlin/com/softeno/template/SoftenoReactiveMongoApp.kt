8bb905b62b5541930232b5c9c822a8b168951b7d
xcodeassociated <janusz.majchrzak@protonmail.com>
2023-01-15 20:47:42 +0100

    add mongo reactive template operation for patch

--- src/main/kotlin/com/softeno/template/SoftenoReactiveMongoApp.kt
+++ src/main/kotlin/com/softeno/template/SoftenoReactiveMongoApp.kt
@@ -8,10 +8,16 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
+import org.springframework.data.mongodb.core.FindAndModifyOptions
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -37,8 +43,7 @@ class RoutesConfig {
 	@Bean
 	fun routes(): RouterFunction<ServerResponse> {
 		return route(GET("/")) { _: ServerRequest
-			->
-			ok().body(BodyInserters.fromObject(MessageDto(message = "hello world")))
+			-> ok().body(BodyInserters.fromObject(MessageDto(message = "hello world")))
 		}
 		// .and(...) <- next router function
 	}
@@ -49,7 +54,10 @@ class RoutesConfig {
 @RestController
 @RequestMapping("/v1/")
 @Validated
-class WebControllerV1(val permissionsReactiveRepository: PermissionsReactiveRepository) {
+class WebControllerV1(
+	val permissionsReactiveRepository: PermissionsReactiveRepository,
+	val permissionsReactiveMongoTemplate: PermissionsReactiveMongoTemplate
+) {
 	@GetMapping("/permissions")
 	fun getAllPermissions(
 		@RequestParam(required = false, defaultValue = "0") page: Int,
@@ -75,11 +83,20 @@ class WebControllerV1(val permissionsReactiveRepository: PermissionsReactiveRepo
 	@PostMapping("/permissions")
 	fun createPermission(@RequestBody input: PermissionInput): Mono<Permission> =
 		permissionsReactiveRepository.save(Permission(id = null, name = input.name, description = input.description))
+
+	@PatchMapping("/permissions/{id}")
+	fun updatePermission(@PathVariable id: String, @RequestBody(required = true) input: PermissionInput): Mono<Permission> =
+		permissionsReactiveMongoTemplate.findAndModify(id, input)
 }
 
-data class MessageDto(val message: String)
+data class MessageDto(
+	val message: String
+)
 
-data class PermissionInput(val name: String, val description: String)
+data class PermissionInput(
+	val name: String,
+	val description: String
+)
 
 @Document
 data class Permission(
@@ -95,4 +112,17 @@ data class Permission(
 @Repository
 interface PermissionsReactiveRepository : ReactiveMongoRepository<Permission, String>, ReactiveQuerydslPredicateExecutor<Permission> {
 	fun findAllBy(pageable: Pageable): Flux<Permission>
+}
+
+@Component
+class PermissionsReactiveMongoTemplate(val mongoTemplate: ReactiveMongoTemplate) {
+	fun findAndModify(id: String, input: PermissionInput): Mono<Permission> {
+		val query = Query(Criteria.where("id").`is`(id))
+		val update = Update().set("name", input.name).set("description", input.description)
+		// note: Update() has more `set` operations based on the document element type to be modified
+		val options = FindAndModifyOptions.options().returnNew(true)
+		// note: can be changed to `upsert`
+		// ref: https://medium.com/geekculture/types-of-update-operations-in-mongodb-using-spring-boot-11d5d4ce88cf
+		return mongoTemplate.findAndModify(query, update, options, Permission::class.java)
+	}
 }
