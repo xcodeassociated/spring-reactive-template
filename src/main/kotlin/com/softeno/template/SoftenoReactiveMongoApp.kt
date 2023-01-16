c04525d414e0f456fb7853bdd107fa4161c2bdce
xcodeassociated <janusz.majchrzak@protonmail.com>
2023-01-16 16:29:16 +0100

    add application ready event listener

--- src/main/kotlin/com/softeno/template/SoftenoReactiveMongoApp.kt
+++ src/main/kotlin/com/softeno/template/SoftenoReactiveMongoApp.kt
@@ -1,7 +1,12 @@
 package com.softeno.template
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.stereotype.Component
 
 
 @SpringBootApplication
@@ -9,4 +14,15 @@ class SoftenoReactiveMongoApp
 
 fun main(args: Array<String>) {
 	runApplication<SoftenoReactiveMongoApp>(*args)
+}
+
+@EnableAsync
+@Component
+class SpringApplicationReadyEventListener {
+	private val logger = LoggerFactory.getLogger(this::class.java)
+
+	@EventListener
+	fun onApplicationReady(event: ApplicationReadyEvent) {
+		logger.info(">> Application Ready")
+	}
 }
