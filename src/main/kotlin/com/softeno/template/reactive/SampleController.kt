824a09c311aab47f4a6582f1204d0d304a162a51
xcodeassociated <janusz.majchrzak@protonmail.com>
2023-01-30 13:32:28 +0100

    refactor idea http files and make them dynamic

--- src/main/kotlin/com/softeno/template/reactive/SampleController.kt
+++ src/main/kotlin/com/softeno/template/reactive/SampleController.kt
@@ -29,39 +29,6 @@ class SampleController(private val sampleService: SampleService) {
 
 }
 
-// OAuth2 secured resource
-@RestController
-@RequestMapping("/sample-secured")
-@Validated
-class SampleSecuredController(private val sampleService: SampleService) {
-    private val log = LogFactory.getLog(javaClass)
-
-    @PostMapping
-    fun postHandler(@RequestBody request: SampleResponseDto): Mono<SampleResponseDto> {
-        log.info("[sample-secured]: POST request: $request")
-        return sampleService.postHandler(request)
-    }
-
-    @GetMapping("/{id}")
-    fun getHandler(@PathVariable id: String): Mono<SampleResponseDto> {
-        log.info("[sample-secured]: GET id: $id")
-        return sampleService.getHandler(id)
-    }
-
-    @PutMapping("/{id}")
-    fun putHandler(@PathVariable id: String, @RequestBody request: SampleResponseDto): Mono<SampleResponseDto> {
-        log.info("[sample-secured]: PUT id: $id, request: $request")
-        return sampleService.putHandler(id, request)
-    }
-
-    @DeleteMapping("/{id}")
-    fun deleteHandler(@PathVariable id: String): Mono<Void> {
-        log.info("[sample-secured]: GET id: $id")
-        return sampleService.deleteHandler(id)
-    }
-
-}
-
 @Service
 class SampleService{
     private val log = LogFactory.getLog(javaClass)
--- src/main/kotlin/com/softeno/template/router/Router.kt
+++ src/main/kotlin/com/softeno/template/router/Router.kt
@@ -1,21 +1,42 @@
 package com.softeno.template.router
 
+import com.softeno.template.reactive.SampleResponseDto
+import com.softeno.template.reactive.SampleService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.*
+import org.springframework.web.reactive.function.server.RequestPredicates.*
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 
 @Configuration
 class RoutesConfig {
     @Bean
     fun routes(): RouterFunction<ServerResponse> {
-        return RouterFunctions.route(RequestPredicates.GET("/")) { _: ServerRequest ->
-            ServerResponse.ok().body(BodyInserters.fromObject(MessageDto(message = "hello world")))
+        return route(GET("/")) { _: ServerRequest ->
+            ok().body(BodyInserters.fromObject(MessageDto(message = "hello world")))
         }
         // .and(...) <- next router function
     }
 
-    // note: there can be other bean router function added here
+    // OAuth2 secured sample resource
+    @Bean
+    fun routes(service: SampleService): RouterFunction<ServerResponse> {
+        return route(GET("/sample-secured/{id}")) { req ->
+            ok().body(service.getHandler(req.pathVariable("id")))
+        }.andRoute(POST("/sample-secured")) { req ->
+            req.bodyToMono(SampleResponseDto::class.java).map {
+                service.postHandler(it)
+            }.flatMap { ok().body(it) }
+        }.andRoute(PUT("/sample-secured/{id}")) { req ->
+            req.bodyToMono(SampleResponseDto::class.java).map {
+                service.putHandler(req.pathVariable("id"), it)
+            }.flatMap { ok().body(it) }
+        }.andRoute(DELETE("/sample-secured/{id}")) { req ->
+            ok().body(service.deleteHandler(req.pathVariable("id")))
+        }
+    }
 }
 
 data class MessageDto(
